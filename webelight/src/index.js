445d8be7211b934431f877fdc8992c33f1df6aed
Atin Sahu <97465321+atin-sahu@users.noreply.github.com>
2022-08-31 23:12:04 +0530

    register login done

--- webelight/src/index.js
+++ webelight/src/index.js
@@ -4,5 +4,10 @@ var cors = require('cors')
 app.use(cors());
 app.use(express.json());
 const productController = require("./controllers/product_controller");
+const userController = require("./controllers/user_controller");
+const {login,register} = require("./controllers/auth_controller");
 app.use("/products",productController );
+app.use("/users",userController);
+app.post("/register", register);
+app.post("/login", login);
 module.exports = app;
