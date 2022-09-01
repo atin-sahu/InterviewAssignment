a0fd4d39b4bdd13ad72ba81afb740ae3d615f094
Atin Sahu <97465321+atin-sahu@users.noreply.github.com>
2022-09-01 12:30:37 +0530

    filter pagination done

--- webelight/src/controllers/product_controller.js
+++ webelight/src/controllers/product_controller.js
@@ -3,7 +3,16 @@ const Product = require("../models/product_model");
 const router = express.Router();
 router.get("/", async (req, res) => {
   try {
-    const products = await Product.find().lean().exec();
+    const page = req.query.page || 1;
+    const page_size = req.query.page_size || 3; 
+    const skip = (page - 1) * page_size;
+    let brand;
+    if(req.query.brand){
+      brand = {brand:req.query.brand}
+    }else{
+      brand = {};
+    }
+    const products = await Product.find(brand).skip(skip).limit(page_size).lean().exec();
     return res.status(200).send(products);
   } catch (error) {
     return res.status(500).send({ message: error.message });
--- webelight/src/models/product_model.js
+++ webelight/src/models/product_model.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 const productSchema = new mongoose.Schema(
     {
         title:{type:String, required:true},
-        price:{type:Number, required:true}
+        price:{type:Number, required:true},
+        brand:{type:String, required:true},
+        category:{type:String, required:true}
     },{
         versionKey:false,
         timestamps:true
